0577584b93b457e3532bdcc61f5a61f7c1dacec8
kidsamort <kidsamort@gmail.com>
2022-04-29 05:59:58 +0300

    jwt add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { TokenAuthGuard } from 'auth/token/tokenAuth.guard';
+import { CreateUserDto } from './dto/createUser-dto';
+import { User } from './user.model';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private usersService: UserService) {}
+
+  @UseGuards(TokenAuthGuard)
+  @Post()
+  createUser(@Body() userDto: CreateUserDto): Promise<User[]> {
+    return this.usersService.getAllUsers();
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/createUser-dto';
+import { User } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(userDto: CreateUserDto): Promise<User> {
+    return await this.userRepository.create(userDto);
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne({ where: { email } });
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepository.findAll({ include: { all: true } });
+  }
+}

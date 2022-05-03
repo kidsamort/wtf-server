413a83ea2711bfce0c880a9f4f4326c94212598d
kidsamort <kidsamort@gmail.com>
2022-05-03 06:38:47 +0300

    deploy heroku

--- src/auth/token/token.controller.ts
+++ src/auth/token/token.controller.ts
@@ -6,13 +6,13 @@ import { TokenService } from './token.service';
 
 @Controller('token')
 export class TokenController {
-  constructor(private tokenServicee: TokenService) {}
+  constructor(private tokenService: TokenService) {}
 
   @Get('/refresh')
   refresh(
     @Body() token: RefreshReqToken,
     @Res({ passthrough: true }) response: Response,
   ): Promise<AuthResponse> {
-    return this.tokenServicee.refresh(token.refreshToken, response);
+    return this.tokenService.refresh(token.refreshToken, response);
   }
 }
